7c836f98a24230c1652a4485459214660a4977da
Harsika Singh <117065981+harsikaSingh@users.noreply.github.com>
2023-01-29 17:13:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 
 ReactDOM.render(
-    
     <BrowserRouter>
         <App />
     </BrowserRouter>
-    ,
-    
-    document.getElementById("root")
-);
+    ,   
+    document.getElementById("root"));
